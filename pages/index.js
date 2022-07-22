ebdeff2d693279b148216dd99a307295099fd5ba
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-07-21 18:22:55 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Text.js!</a>
+          Welcome to <a href="https://nextjs.org">Hello Brian!</a>
         </h1>
 
         <p className={styles.description}>
