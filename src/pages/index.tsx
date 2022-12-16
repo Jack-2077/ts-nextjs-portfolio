df43ff1b562ec2d796590022dc8eed53ca22609d
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-12-15 17:28:02 -0800

    append elements to the head of the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { GetStaticProps } from 'next';
+import Head from 'next/head';
 import { Home, About, Projects, Skills, Contact } from '../components/sections';
-import Navbar from '../components/common/Navbar';
-import Footer from '../components/common/Footer';
+import { Navbar, Footer } from '../components/common';
 import { projectsData, type projects } from '../services/data-svc';
 
 interface Props {
@@ -11,6 +11,28 @@ interface Props {
 const HomePage = ({ projectsData }: Props) => {
   return (
     <div className='flex flex-col justify-between text-white layout bg-shade-1'>
+      <Head>
+        <title>Jack John</title>
+        <meta name='description' content='Full Stack Developer' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/favicon/site.webmanifest' />
+      </Head>
       <Navbar />
       <Home />
       <Projects data={projectsData} />
